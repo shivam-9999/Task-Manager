ebe19166afb382a0b99151785d51fed908dc7fe8
shivam <shivammaniya7469@gmail.com>
2023-02-28 12:25:19 -0500

    SERVER DONE WITHOUT ISSUE

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,5 @@
 //Development configuration options
 //To sign the session identifier, use a secret string
-
 module.exports = {
     db: 'mongodb://localhost/TaskManager',
     sessionSecret: 'developmentSession-nSecret'
