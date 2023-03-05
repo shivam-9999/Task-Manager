7e80d3d2909a57cd760e130924843df92565a0cc
shivam <shivammaniya7469@gmail.com>
2023-03-04 23:28:28 -0500

    Final commit

--- react-task-client/src/Components/Home.js
+++ react-task-client/src/Components/Home.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Card, Container } from "react-bootstrap";
 
 // import  pic from "./../images/sample_test.png";
-function Home(props)
-{
-
-    return (
-        <div>
-            {/* <img src={pic} /> */}
-            <h2> Express - React with CRUD Operations</h2>
-            <p>React front-end calls Express REST API to add, 
-            list, update, or delete a task.</p>
-        </div>
-    );
-
+function Home(props) {
+  return (
+    <Container>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          color: "darkslategray",
+        }}
+      >
+        Welcome To my application
+      </h1>
+      <h5
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          color: "darkslategray",
+          color: "#6e1f67",
+        }}
+      >
+        Sign In to get access to functionalitities
+      </h5>
+    </Container>
+  );
 }
-// withRouter will pass updated match, location, and history props 
+// withRouter will pass updated match, location, and history props
 // to the wrapped component whenever it renders.
 export default Home;
